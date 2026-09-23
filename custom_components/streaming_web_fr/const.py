312b407b6ee0352eb03ba50a5fc4eddcad57d627
PourLePlaisir-HA <seb.github.project@gmail.com>
2026-09-23 11:13:36 +0200

    Align runtime version with 0.5.0

--- custom_components/streaming_web_fr/const.py
+++ custom_components/streaming_web_fr/const.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 DOMAIN = "streaming_web_fr"
 NAME = "Streaming Web FR"
-VERSION = "0.1.0-beta.4"
+VERSION = "0.5.0"
 
 CONF_PROVIDERS = "providers"
 CONF_PLAYERS = "players"
