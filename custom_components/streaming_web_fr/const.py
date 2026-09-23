a73addaeba58d49c7444f4c129108dc348a92b65
PourLePlaisir-HA <seb.github.project@gmail.com>
2026-09-23 09:56:42 +0200

    Add integration constants

---
+++ custom_components/streaming_web_fr/const.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+DOMAIN = "streaming_web_fr"
+NAME = "Streaming Web FR"
+VERSION = "0.1.0-beta.1"
+
+CONF_PROVIDERS = "providers"
+CONF_PLAYERS = "players"
+
+DEFAULT_PROVIDER_TYPE = "drabam"
+SUPPORTED_PROVIDER_TYPES = ("drabam",)
+
+AUTH_NONE = "none"
+AUTH_BASIC = "basic"
+AUTH_API_KEY = "api_key"
+AUTH_BEARER = "bearer"
+AUTH_COOKIE = "cookie"
+AUTH_CUSTOM_HEADERS = "custom_headers"
+
+AUTH_MODES = (
+    AUTH_NONE,
+    AUTH_BASIC,
+    AUTH_API_KEY,
+    AUTH_BEARER,
+    AUTH_COOKIE,
+    AUTH_CUSTOM_HEADERS,
+)
+
+VLC_ANDROID_PACKAGE = "org.videolan.vlc"
+VLC_ANDROID_ACTIVITY = "org.videolan.vlc/.StartActivity"
+
+CARD_RESOURCE_PATH = "/streaming_web_fr/streaming-web-fr-card.js"
