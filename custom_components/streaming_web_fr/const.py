98aaf2ad65385fa28d28e68bb379339effa5762d
PourLePlaisir-HA <seb.github.project@gmail.com>
2026-09-23 10:07:39 +0200

    Enable form-login authentication mode

--- custom_components/streaming_web_fr/const.py
+++ custom_components/streaming_web_fr/const.py
@@ -12,6 +12,7 @@ SUPPORTED_PROVIDER_TYPES = ("drabam",)
 
 AUTH_NONE = "none"
 AUTH_BASIC = "basic"
+AUTH_FORM_LOGIN = "form_login"
 AUTH_API_KEY = "api_key"
 AUTH_BEARER = "bearer"
 AUTH_COOKIE = "cookie"
@@ -20,6 +21,7 @@ AUTH_CUSTOM_HEADERS = "custom_headers"
 AUTH_MODES = (
     AUTH_NONE,
     AUTH_BASIC,
+    AUTH_FORM_LOGIN,
     AUTH_API_KEY,
     AUTH_BEARER,
     AUTH_COOKIE,
